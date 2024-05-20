3ba88506c2bbd5dec64452729fe0fce32654af1d
top2429 <top2429@outlook.com>
2024-05-20 19:42:10 -0400

    fix: Removed the print.

--- lib/commonWidgets/arrowButton.dart
+++ lib/commonWidgets/arrowButton.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -23,7 +25,9 @@ class ArrowButton extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(24),
-            color: plusIcon?  const Color.fromARGB(255, 247, 245, 245):const Color.fromARGB(255, 240, 240, 240)),
+            color: plusIcon
+                ? const Color.fromARGB(255, 247, 245, 245)
+                : const Color.fromARGB(255, 240, 240, 240)),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
           child: Row(
@@ -33,17 +37,22 @@ class ArrowButton extends StatelessWidget {
                       color: innerContainerColor, shape: BoxShape.circle),
                   child: Padding(
                     padding: const EdgeInsets.all(12),
-                    child: SvgPicture.asset("assets/images/$image",height: 19, width: 19,),
+                    child: SvgPicture.asset(
+                      "assets/images/$image",
+                      height: 19,
+                      width: 19,
+                    ),
                   )),
               const SizedBox(
                 width: 20,
               ),
               Text(
                 label,
-                style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
               ),
               const Spacer(),
-              plusIcon?const Icon(Icons.add): const Icon(Icons.arrow_forward)
+              plusIcon ? const Icon(Icons.add) : const Icon(Icons.arrow_forward)
             ],
           ),
         ),
