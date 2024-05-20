5e9b2013b56537d7da6af76c8f6605ec9d6b4d32
top2429 <top2429@outlook.com>
2024-05-20 16:42:19 -0400

    Co-authored-by: kco1206 <kco1206@outlook.com>

--- lib/features/profile/presentation/profileScreen.dart
+++ lib/features/profile/presentation/profileScreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names, use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
