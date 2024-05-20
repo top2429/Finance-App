2a54006901ebe085642262dc59e2f664deec7c0a
top2429 <top2429@outlook.com>
2024-05-20 16:58:34 -0400

    Co-authored-by: kco1206 <kco1206@outlook.com>

--- lib/features/savingGoals/presentation/dotted_widget.dart
+++ lib/features/savingGoals/presentation/dotted_widget.dart
@@ -14,30 +14,34 @@ class _CreateSavingGoalDottedState extends State<CreateSavingGoalDotted> {
       height: 60,
       width: 320,
       decoration: BoxDecoration(
-          color: Color.fromARGB(255, 235, 233, 233),
+          color: const Color.fromARGB(255, 235, 233, 233),
           borderRadius: BorderRadius.circular(5)),
-      child: Stack(
-        children: [
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Create a new saving goal",style: TextStyle(fontSize: 17),),
-                SizedBox(width: 15,),
-                Icon(Icons.add),
-              ],
-            ),
+      child: Stack(children: [
+        const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Create a new saving goal",
+                style: TextStyle(fontSize: 17),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Icon(Icons.add),
+            ],
           ),
-          Positioned(
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: CustomPaint(
+        ),
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: CustomPaint(
             painter: DottedRectPainter(),
-                  ),
           ),
-     ] ),
+        ),
+      ]),
     );
   }
 }
@@ -46,12 +50,12 @@ class DottedRectPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
-      ..color = Color.fromARGB(255, 87, 87, 87) // Border color
+      ..color = const Color.fromARGB(255, 87, 87, 87) // Border color
       ..style = PaintingStyle.stroke // Border style
       ..strokeWidth = 2.0; // Border width
 
-    final double dashWidth = 7; // Adjust the width of each dot
-    final double dashSpace = 3; // Adjust the space between dots
+    const double dashWidth = 7; // Adjust the width of each dot
+    const double dashSpace = 3; // Adjust the space between dots
     double currentX = 0;
 
     // Draw dotted lines on the top and bottom sides
@@ -69,7 +73,7 @@ class DottedRectPainter extends CustomPainter {
       currentX += dashWidth + dashSpace;
     }
 
-    final double dashHeight = 5; // Adjust the height of each dot
+    const double dashHeight = 5; // Adjust the height of each dot
     double currentY = 0;
 
     // Draw dotted lines on the left and right sides, creating the corners
@@ -93,7 +97,3 @@ class DottedRectPainter extends CustomPainter {
     return false;
   }
 }
-
-
-
-
