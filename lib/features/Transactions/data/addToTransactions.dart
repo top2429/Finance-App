61ddae30b08345c01a3bf773393b2d362ce1d4c1
top2429 <top2429@outlook.com>
2024-05-20 19:54:01 -0400

    fix: Removed the print.

--- lib/features/Transactions/data/addToTransactions.dart
+++ lib/features/Transactions/data/addToTransactions.dart
@@ -1,9 +1,27 @@
+// ignore_for_file: file_names
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tai/features/Transactions/domain/transactionsModel.dart';
 
-addToTransactions(String senderId,String senderName,String senderImage, String receiverId,String receiverName,String receiverImage, double amount,String time ) async {
-      var db = FirebaseFirestore.instance;
-      TransactionsModel transactionsModel = TransactionsModel(senderId,senderName ,senderImage,receiverId,receiverName, receiverImage ,amount, time,Timestamp.now());
-      await db.collection("transactions").add(transactionsModel.toJson());
+addToTransactions(
+    String senderId,
+    String senderName,
+    String senderImage,
+    String receiverId,
+    String receiverName,
+    String receiverImage,
+    double amount,
+    String time) async {
+  var db = FirebaseFirestore.instance;
+  TransactionsModel transactionsModel = TransactionsModel(
+      senderId,
+      senderName,
+      senderImage,
+      receiverId,
+      receiverName,
+      receiverImage,
+      amount,
+      time,
+      Timestamp.now());
+  await db.collection("transactions").add(transactionsModel.toJson());
 }
